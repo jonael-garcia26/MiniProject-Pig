802f409b869f8d226eebc01d75765403da1e0724
jonael-garcia26 <jonael.garcia@upr.edu>
2023-08-27 02:23:38 -0400

    Progres stage 2 & testing code

--- Pig.py
+++ Pig.py
@@ -1,7 +1,7 @@
 import random
 
 def dice():
-    min_num = 1
+    min_num = 2
     max_num = 6
     rand_num = random.randint(min_num, max_num)
 
@@ -23,13 +23,32 @@ while True:
         print("\nInvalido, intente nuevamente.\n")
 
 print(f"\nPerfecto!! son {player_num} jugadores")
-print("***************************************************************\n")
 
 final_score = 50
 dict_scores = {}
 
+#****************************BROKEN CODE******************************
 while True:
     for i in range(player_num):
-        print(f"Player #{str(i + 1)}:\n")
-        input("Do you want to roll the dice? (y/n): ")
+        print("------------------------------------------------------------")
+        print(f"\nJugador #{str(i + 1)}:\n")
+        dice_roll = str.strip(str.lower(input("Quieres tirar el dado? (y/n): ")))
+        player_score = 0
+        
+        if dice_roll == "n":
+            continue
+        if dice_roll == "y":
+            dice_num = dice()
+            print("\n" + dice_num)
+            if dice_num == 1:
+                player_score = 0
+            else:
+                dict_scores[f"Jugador #{str(i + 1)}"] = dice_num
+                print(dict_scores)
 
+#***************************TEST CODE***************************************
+
+# x = 4
+# for i in range(x):
+#     print(i)
+#     if f"{i}" == f"{range(x)[-1]}":
