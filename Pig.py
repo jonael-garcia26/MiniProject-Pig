2a702f3e468bce3a63c02e072a7f538033f26cd0
jonael-garcia26 <jonael.garcia@upr.edu>
2023-09-02 16:00:24 -0400

    Fix Dictionary of players

--- Pig.py
+++ Pig.py
@@ -7,6 +7,9 @@ def dice():
 
     return rand_num
 
+final_score = 50
+dict_scores = {}
+
 while True:
     print("----------------------------------------------------------\n")
     player_num = input("Selecciona la cantidad de jugadores (2-4): ")
@@ -15,6 +18,8 @@ while True:
         player_num = int(player_num)
 
         if player_num >= 2 and player_num <= 4:
+            for i in range(player_num):
+                dict_scores[f"Jugador #{i + 1}"] = 0
             break
 
         else:
@@ -24,31 +29,49 @@ while True:
 
 print(f"\nPerfecto!! son {player_num} jugadores")
 
-final_score = 50
-dict_scores = {}
+while True:
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #****************************BROKEN CODE******************************
-while True:
-    for i in range(player_num):
-        print("------------------------------------------------------------")
-        print(f"\nJugador #{str(i + 1)}:\n")
-        dice_roll = str.strip(str.lower(input("Quieres tirar el dado? (y/n): ")))
-        player_score = 0
+# while True:
+#     for i in range(player_num):
+#         print("------------------------------------------------------------")
+#         print(f"\nJugador #{str(i + 1)}:\n")
+#         dice_roll = str.strip(str.lower(input("Quieres tirar el dado? (y/n): ")))
+#         player_score = 0
         
-        if dice_roll == "n":
-            continue
-        if dice_roll == "y":
-            dice_num = dice()
-            print("\n" + dice_num)
-            if dice_num == 1:
-                player_score = 0
-            else:
-                dict_scores[f"Jugador #{str(i + 1)}"] = dice_num
-                print(dict_scores)
+#         if dice_roll == "n":
+#             continue
+#         if dice_roll == "y":
+#             dice_num = dice()
+#             print("\n" + dice_num)
+#             if dice_num == 1:
+#                 player_score = 0
+#             else:
+#                 dict_scores[f"Jugador #{str(i + 1)}"] = dice_num
+#                 print(dict_scores)
 
 #***************************TEST CODE***************************************
 
 # x = 4
 # for i in range(x):
 #     print(i)
-#     if f"{i}" == f"{range(x)[-1]}":
+#     if f"{i}" == f"{range(x)[-1]}"
