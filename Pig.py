5741de59f5eb05bfaa581ee24af46cff6f4e36f5
jonael-garcia26 <jonael.garcia@upr.edu>
2023-09-02 18:30:13 -0400

    Finishing touches to mini game - Done

--- Pig.py
+++ Pig.py
@@ -1,14 +1,21 @@
 import random
+import time
 
 def dice():
-    min_num = 2
+    min_num = 1
     max_num = 6
     rand_num = random.randint(min_num, max_num)
 
     return rand_num
 
 final_score = 50
-dict_scores = {}
+players_list = []
+cicle = 0
+
+def scoreboard():
+    print("\n\nSCORE BOARD:")
+    for i in players_list:
+        print(f"Jugador #{i[0]}: {i[1]} pts.")
 
 while True:
     print("----------------------------------------------------------\n")
@@ -18,8 +25,10 @@ while True:
         player_num = int(player_num)
 
         if player_num >= 2 and player_num <= 4:
+
             for i in range(player_num):
-                dict_scores[f"Jugador #{i + 1}"] = 0
+                players_list.append([i + 1, 0])
+
             break
 
         else:
@@ -28,50 +37,47 @@ while True:
         print("\nInvalido, intente nuevamente.\n")
 
 print(f"\nPerfecto!! son {player_num} jugadores")
+time.sleep(1)
 
 while True:
-    
 
+    for i in players_list:
+        if i[1] >= final_score:
+            print(f"\nEl JUGADOR #{i[0]} GANA")
+            scoreboard()
+            time.sleep(5)
+            exit()
 
+    if cicle >= player_num:
+        cicle = 0
+    print("\n-------------------------------------------------\n")
+    print(f"turno del Jugador #{players_list[cicle][0]}\n")
+    score = 0
 
+    while True:
+        play = str.strip(str.lower(input("\nQuieres tirar el dado? (y/n) ")))
 
+        if play != "y":
+            players_list[cicle][1] += score
+            break
+        else:
+            num = dice()
+            print(f"El dado saco un {num}\n")
 
+            if num != 1:
+                score += num
+                if score >= final_score:
+                    players_list[cicle][1] += score
+                    print(f"\nEl JUGADOR #{players_list[cicle][0]} GANA")
+                    scoreboard()
+                    time.sleep(5)
+                    exit()
+                print(f"Puntuacion por turno: {score}\n")
 
+            else:
+                score = 0
+                print("oh! Perdiste tus puntos y turno")
+                break
 
-
-
-
-
-
-
-
-
-
-
-
-
-#****************************BROKEN CODE******************************
-# while True:
-#     for i in range(player_num):
-#         print("------------------------------------------------------------")
-#         print(f"\nJugador #{str(i + 1)}:\n")
-#         dice_roll = str.strip(str.lower(input("Quieres tirar el dado? (y/n): ")))
-#         player_score = 0
-        
-#         if dice_roll == "n":
-#             continue
-#         if dice_roll == "y":
-#             dice_num = dice()
-#             print("\n" + dice_num)
-#             if dice_num == 1:
-#                 player_score = 0
-#             else:
-#                 dict_scores[f"Jugador #{str(i + 1)}"] = dice_num
-#                 print(dict_scores)
-
-#***************************TEST CODE***************************************
-
-# x = 4
-# for i in range(x):
-#     print(i)
-#     if f"{i}" == f"{range(x)[-1]}"
+    scoreboard()
+    cicle += 1
